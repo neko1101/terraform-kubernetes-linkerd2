3958dee980d0b81550b53c17d9ecbae353fcbe83
Hafiz Nor <38586112+neko1101@users.noreply.github.com>
2024-05-22 03:02:42 +0800

    fix: fmt

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">= 2.30.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = ">= 2.13.2"
     }
     time = {
