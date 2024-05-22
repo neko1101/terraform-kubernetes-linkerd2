652d4a0795ecc351b360fe33129c6970a8b05e96
Hafiz Nor <38586112+neko1101@users.noreply.github.com>
2024-05-22 23:11:45 +0800

    chores: vars improvement

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "kubernetes" {
-  description = "Kubernetes connection configuration"
-  type = object({
-    host : string,
-    cluster_ca_certificate : string,
-    token : string,
-  })
+  description = "Kubernetes config"
+  type        = map(string)
+  default = {
+    "config_path"    = "~/.kube/config"
+    "config_context" = "my-context"
+  }
 }
 
 variable "linkerd_repository" {
@@ -16,8 +16,9 @@ variable "linkerd_repository" {
 variable "control_plane_helm_version" {
   description = "Control plane helm version"
   type        = string
-  default     = "1.16.11"
+  default     = "1.16.10"
 }
+
 variable "control_plane_namespace" {
   description = "Control plane namespace"
   type        = string
@@ -42,6 +43,60 @@ variable "control_plane_cert_renew_before" {
   default     = "24h0m0s"
 }
 
+variable "control_plane_replica_count" {
+  description = "Control plane replica count"
+  type        = number
+  default     = 1
+}
+
+variable "tap_replica_count" {
+  description = "Tap replica count"
+  type        = number
+  default     = 1
+}
+
+variable "tap_injector_replica_count" {
+  description = "Tap injector replica count"
+  type        = number
+  default     = 1
+}
+
+variable "dashboard_replica_count" {
+  description = "Dashboard replica count"
+  type        = number
+  default     = 1
+}
+
+variable "metrics_replica_count" {
+  description = "Metrics api replica count"
+  type        = number
+  default     = 1
+}
+
+variable "viz_enable_pod_anti_affinity" {
+  description = "Viz enable podAntiAffinity"
+  type        = bool
+  default     = false
+}
+
+variable "viz_enable_pod_distruption_budget" {
+  description = "Viz enable podDisruptionBudget"
+  type        = bool
+  default     = false
+}
+
+variable "control_plane_enable_pod_anti_affinity" {
+  description = "Control plane enable podAntiAffinity"
+  type        = bool
+  default     = false
+}
+
+variable "control_plane_enable_pod_distruption_budget" {
+  description = "Control plane enable podDisruptionBudget"
+  type        = bool
+  default     = false
+}
+
 variable "webhook_ca_validity" {
   description = "Webhook Issuer CA validity in hours eg: 175200 for 20 years"
   type        = string
@@ -62,7 +117,7 @@ variable "webhook_cert_renew_before" {
 variable "viz_helm_version" {
   description = "Viz helm version"
   type        = string
-  default     = "30.12.11"
+  default     = "30.12.10"
 }
 
 variable "viz_namespace" {
