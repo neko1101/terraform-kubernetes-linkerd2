c7c4deb111d66c1bbae6792e4ca1627ed03ae94d
Hafiz Nor <38586112+neko1101@users.noreply.github.com>
2024-05-22 23:15:00 +0800

    Revert "Merge branch 'main' into variables-improvements"
    This reverts commit 989b0adbc2a339ad1250fa54c3ad4aab78452f51, reversing
    changes made to 652d4a0795ecc351b360fe33129c6970a8b05e96.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
 variable "kubernetes" {
-  type = object({
-    host : string,
-    cluster_ca_certificate : string,
-    token : string,
-  })
+  description = "Kubernetes config"
+  type        = map(string)
+  default = {
+    "config_path"    = "~/.kube/config"
+    "config_context" = "my-context"
+  }
 }
 
 variable "linkerd_repository" {
@@ -24,6 +25,95 @@ variable "control_plane_namespace" {
   default     = "linkerd"
 }
 
+variable "control_plane_ca_validity" {
+  description = "Control plane Issuer CA validity in hours eg: 175200 for 20 years"
+  type        = string
+  default     = "175200"
+}
+
+variable "control_plane_cert_duration" {
+  description = "Control plane TLS cert duration eg: 24h0m0s"
+  type        = string
+  default     = "72h0m0s"
+}
+
+variable "control_plane_cert_renew_before" {
+  description = "Control plane TLS cert renew before eg: 1h0m0s"
+  type        = string
+  default     = "24h0m0s"
+}
+
+variable "control_plane_replica_count" {
+  description = "Control plane replica count"
+  type        = number
+  default     = 1
+}
+
+variable "tap_replica_count" {
+  description = "Tap replica count"
+  type        = number
+  default     = 1
+}
+
+variable "tap_injector_replica_count" {
+  description = "Tap injector replica count"
+  type        = number
+  default     = 1
+}
+
+variable "dashboard_replica_count" {
+  description = "Dashboard replica count"
+  type        = number
+  default     = 1
+}
+
+variable "metrics_replica_count" {
+  description = "Metrics api replica count"
+  type        = number
+  default     = 1
+}
+
+variable "viz_enable_pod_anti_affinity" {
+  description = "Viz enable podAntiAffinity"
+  type        = bool
+  default     = false
+}
+
+variable "viz_enable_pod_distruption_budget" {
+  description = "Viz enable podDisruptionBudget"
+  type        = bool
+  default     = false
+}
+
+variable "control_plane_enable_pod_anti_affinity" {
+  description = "Control plane enable podAntiAffinity"
+  type        = bool
+  default     = false
+}
+
+variable "control_plane_enable_pod_distruption_budget" {
+  description = "Control plane enable podDisruptionBudget"
+  type        = bool
+  default     = false
+}
+
+variable "webhook_ca_validity" {
+  description = "Webhook Issuer CA validity in hours eg: 175200 for 20 years"
+  type        = string
+  default     = "175200"
+}
+
+variable "webhook_cert_duration" {
+  description = "Webhook TLS cert duration eg: 24h0m0s"
+  type        = string
+  default     = "48h0m0s"
+}
+
+variable "webhook_cert_renew_before" {
+  description = "Webhook TLS cert renew before eg: 1h0m0s"
+  type        = string
+  default     = "24h0m0s"
+}
 variable "viz_helm_version" {
   description = "Viz helm version"
   type        = string
